65d7f8b0919720da37949a15ba8eddd943c68be8
Joao Victor Lopes Padilha <105240389+JoaoVLPadilha@users.noreply.github.com>
2023-07-03 21:38:17 -0300

    feat: add initial presentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Nav from '@components/Nav';
 import '@styles/globals.css';
 
 export const metadata = {
@@ -14,7 +15,10 @@ const RootLayout = ({ children }: IRootLayout) => {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+            <Nav/>
+          {children}
+          </main>
       </body>
     </html>
   );
